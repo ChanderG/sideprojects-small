b0c532b05cb4b1d035124e2c711485fad45bdfec
ChanderG <chandergovind@gmail.com>
2016-02-12 10:07:02 +0530

    try out simple sqlit in memory mapping

--- sinatra-with-datamapper/server.rb
+++ sinatra-with-datamapper/server.rb
@@ -1,6 +1,29 @@
 require "sinatra"
 require "data_mapper"
 
+DataMapper::Logger.new($stdout,	:debug)
+
+DataMapper.setup(:default, 'sqlite::memory:')
+
+class Post
+  include DataMapper::Resource
+
+  property :id,		Serial
+  property :title,	String
+end
+
+# to finalize models
+DataMapper.finalize
+
+# to setup database with specified structure
+DataMapper.auto_migrate!
+
+Post.create(:title => 'Hello World')
+
 get "/" do
   "Hello world"
+end
+
+get "/posts" do
+  Post.all.to_json
 end
