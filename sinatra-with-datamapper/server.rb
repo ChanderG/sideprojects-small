30f5866adc2f44966aeb46bfdaa35f8199774396
ChanderG <chandergovind@gmail.com>
2016-02-12 10:22:17 +0530

    simple relations tested

--- sinatra-with-datamapper/server.rb
+++ sinatra-with-datamapper/server.rb
@@ -10,6 +10,17 @@ class Post
 
   property :id,		Serial
   property :title,	String
+
+  has n, :comments
+end
+
+class Comment
+  include DataMapper::Resource
+
+  property :id,		Serial
+  property :author, String
+
+  belongs_to :post
 end
 
 # to finalize models
@@ -18,7 +29,11 @@ DataMapper.finalize
 # to setup database with specified structure
 DataMapper.auto_migrate!
 
-Post.create(:title => 'Hello World')
+p1 = Post.create(:title => 'Hello World')
+Post.create(:title => 'First Post')
+
+p1.comments.create(:author => 'John')
+Post.get(2).comments.create(:author => 'Harold')
 
 get "/" do
   "Hello world"
@@ -26,4 +41,16 @@ end
 
 get "/posts" do
   Post.all.to_json
+end
+
+get "/post/:id" do
+  Post.get(params['id']).to_json
+end
+
+get "/comments" do
+  Comment.all.to_json
+end
+
+get "/comments/:id" do
+  Comment.get(params['id']).to_json
 end
