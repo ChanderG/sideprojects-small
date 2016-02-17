00971ca48b075247a184af36d6b36798bb6ad20b
ChanderG <chandergovind@gmail.com>
2016-02-17 21:43:55 +0530

    read file and process words in go

--- hw-go/hangman.go
+++ hw-go/hangman.go
@@ -1,21 +1,57 @@
 package main
 
-import "fmt"
-import "os"
+import (
+  "fmt"
+  "io/ioutil"
+  "strings"
+  "math/rand"
+  "time"
+)
+
+func removeApostophe(s string) string {
+  return strings.Split(s, "'")[0]
+}
 
 func main(){
   fmt.Println("Welcome to hangman game.")
-  var filename string = "sample"
 
-  fd, err := os.Open(filename)
+  fmt.Println("Choosing random word...")
+  word := chooseWord()
+  fmt.Println("word is", word)
+}
+func chooseWord() string {
+  var filename string = "words"
+
+  // read entire file contents
+  fmt.Println("Reading file", filename)
+  dat, err := ioutil.ReadFile(filename)
   if err!= nil {
 	panic(err)
   }
 
-  fmt.Println("Opened file", filename)
+  // convert data to string
+  lines := string(dat)
+  //fmt.Println(lines)
 
-  err = fd.Close()
-  if err != nil {
-	panic(err)
+  //s := make([]string, 0)
+  s := strings.Split(lines, "\n")
+
+  // convert to lower case words
+  for i:=0; i<len(s);i++ {
+	s[i] = strings.ToLower(s[i])
   }
+
+  // remove ' stuff
+  for i:=0; i<len(s); i++ {
+	s[i] = removeApostophe(s[i])
+  }
+
+  // init seed for actual random performance
+  s1 := rand.NewSource(time.Now().UnixNano())
+  r1 := rand.New(s1)
+
+  // choose random value
+  var word string = s[r1.Intn(len(s))]
+
+  return word
 }
