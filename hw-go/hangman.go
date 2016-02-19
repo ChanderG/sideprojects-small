890d13dc6dffb066c612340b28bd2468b0848465
ChanderG <chandergovind@gmail.com>
2016-02-19 16:53:45 +0530

    complete hangman game
    with the same quirks as in the scala version

--- hw-go/hangman.go
+++ hw-go/hangman.go
@@ -6,6 +6,8 @@ import (
   "strings"
   "math/rand"
   "time"
+  "bufio"
+  "os"
 )
 
 func removeApostophe(s string) string {
@@ -32,15 +34,62 @@ func blankifyWord(s string) string {
   return blankedWord
 }
 
+/* 
+ * Prompt for an input char, run on word and return new word.
+ */
+func guessLetter(actual string, curr string) string {
+  // scan for an input
+  fmt.Print("> ")
+  // read one line input
+  b := bufio.NewReader(os.Stdin)
+  input, err := b.ReadString('\n')
+  if err != nil {
+	panic(err)
+  }
+  char_guess := []rune(input)[0]
+
+  curr_list := []rune(curr)
+  actual_list := []rune(actual)
+
+  for i:=0; i< len(curr_list);i++ {
+	if actual_list[i] == char_guess {
+	  // illuminate
+	  curr_list[i] = char_guess
+	}
+  }
+  return string(curr_list)
+}
+
+func playGame(actual string, word string, n int) {
+  curr := word
+  for i:=0; i< n; i++ {
+	fmt.Println(curr)
+	curr = guessLetter(actual, curr)
+  }
+  if curr == actual{
+	fmt.Println("Good job. You won.")
+  } else {
+	fmt.Println("Word was", actual)
+	fmt.Println("Hard Luck. Try again.")
+  }
+}
+
 func main(){
   fmt.Println("Welcome to hangman game.")
 
   fmt.Println("Choosing random word...")
   word := chooseWord()
-  fmt.Println("word is", word)
+  //fmt.Println("word is", word)
+  /*
+  Partially correct way to find length of word.
+  Still need to figure out a way to loop over chars
+  fmt.Println("len of word: ", len([]rune(word)))
+  */
 
-  word = blankifyWord(word)
-  fmt.Println(word)
+  //fmt.Println(word)
+  newword := blankifyWord(word)
+
+  playGame(word, newword, 3)
 }
 
 func chooseWord() string {
