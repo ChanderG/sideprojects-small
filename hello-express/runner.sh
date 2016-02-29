997141fc5a843265bbac972531ca978b6a15e4dd
ChanderG <chandergovind@gmail.com>
2016-02-29 13:27:10 +0530

    hello world in express

---
+++ hello-express/runner.sh
@@ -0,0 +1,11 @@
+if [ "$1" == "" ]; then
+  echo "Incorrect usage."
+  echo "./runner.sh <option> where option one of:"
+  echo "  cli: start docker container"
+  exit
+fi
+
+if [ "$1" == "cli" ]; then
+  docker run -it --rm --net=host -v `pwd`:/ws -w /ws -e http_proxy="http://10.3.100.207:8080/" -e https_proxy="http://10.3.100.207:8080" node /bin/bash
+  exit
+fi
